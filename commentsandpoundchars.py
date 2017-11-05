e1cae831755506b08a44405292e1e92c9c80fb4f
root <root@chefnode.example.com>
2017-11-05 02:06:39 -0500

    commentsandpoundchars.py

---
+++ commentsandpoundchars.py
@@ -0,0 +1,6 @@
+# A comment, this is so you can read your program later.
+# Anything after this is ignored by python
+print("I could have code like this") # and the comment
+# you can also use a comment to "disable" or comment out
+# print ("This won't run.")
+print ("this will run.")
