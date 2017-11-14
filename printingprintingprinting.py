f85d2c05e32b97e5a735086448d0403072d0c054
sanjaypandrala <sanjay3902@gmail.com>
2017-11-14 00:37:13 -0500

    printingprintingprinting.py

---
+++ printingprintingprinting.py
@@ -0,0 +1,13 @@
+# Here's some new strange stuff, remember type it exactly.
+
+days = "Mon Tue Wed Thr Fri Sat Sun"
+months = "Jan\nFeb\nMar\nApr\nMay\nJun\nJul\nAug\nSep\nOct\nNov\nDec"
+
+print ("Here are the days: ", days)
+print("Here are the months: ", months )
+
+print( """
+There is some going on here. with the three double-quote.
+we'll be able to type as much as we like.
+Even 4 lines if we want, , 5, or 6.
+""")
