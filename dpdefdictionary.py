9b8eb3253483f79e7fc84d075cea492ba2d0c3a6
sanjaypandrala <sanjay3902@gmail.com>
2017-11-14 00:17:11 -0500

    till ex8

---
+++ dpdefdictionary.py
@@ -0,0 +1,9 @@
+myname = {"fname": "naga", "mname":"sanjay", "lname":"pandrala"}
+print(myname)
+print(myname["fname"])
+print(myname["lname"])  \
+#print(myname["sanjay"])
+print("my first name as {myname['fname']}")
+print("my last name as {myname['lname']}")
+print("my first name as myname.fname")
+print("my last name as myname.lname")
